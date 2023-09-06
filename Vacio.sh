fb54ce4c0f7455bd82f5755219155e5ba061f9d3
FedericoD3 <fvduran@gmail.com>
2023-09-06 14:10:02 -0400

    Update del 2023-09-06_14:10

--- Vacio.sh
+++ Vacio.sh
@@ -3,7 +3,7 @@
 LogVacio=$1
 
 # Lienzo vacío transparente (inc margenes y bordes):
-magick -size 880x342 xc:none $LogVacio
+magick -size 737x342 xc:none $LogVacio
 
 # Agregar lineas horizontales cada 5 dias:
 magick $LogVacio -stroke yellow -draw 'line 16,11 736,11' $LogVacio
@@ -16,29 +16,29 @@ magick $LogVacio -stroke yellow -draw 'line 16,330 736,330' $LogVacio
 magick $LogVacio -stroke yellow -draw 'line 16,341 736,341' $LogVacio
 
 # Agregar lineas verticales cada hora (6 bloques)
-magick $LogVacio -stroke yellow -draw 'line 16,11 16,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 41,11 41,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 71,11 71,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 101,11 101,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 131,11 131,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 161,11 161,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 191,11 191,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 221,11 221,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 251,11 251,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 281,11 281,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 311,11 311,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 341,11 341,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 371,11 371,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 401,11 401,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 431,11 431,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 461,11 461,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 491,11 491,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 521,11 521,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 551,11 551,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 581,11 581,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 611,11 611,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 641,11 641,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 671,11 671,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 701,11 701,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 731,11 731,341' $LogVacio
-magick $LogVacio -stroke yellow -draw 'line 736,11 736,341' $LogVacio
+magick $LogVacio -stroke yellow -draw 'line 16,11 16,341' $LogVacio	# Despues del minuto 0
+magick $LogVacio -stroke yellow -draw 'line 46,11 46,341' $LogVacio	# Despues del minuto 60
+magick $LogVacio -stroke yellow -draw 'line 76,11 76,341' $LogVacio	# Despues del minuto 120
+magick $LogVacio -stroke yellow -draw 'line 106,11 106,341' $LogVacio	# Despues del minuto 180
+magick $LogVacio -stroke yellow -draw 'line 136,11 136,341' $LogVacio	# Despues del minuto 240
+magick $LogVacio -stroke yellow -draw 'line 166,11 166,341' $LogVacio	# Despues del minuto 300
+magick $LogVacio -stroke yellow -draw 'line 196,11 196,341' $LogVacio	# Despues del minuto 360
+magick $LogVacio -stroke yellow -draw 'line 226,11 226,341' $LogVacio	# Despues del minuto 420
+magick $LogVacio -stroke yellow -draw 'line 256,11 256,341' $LogVacio	# Despues del minuto 480
+magick $LogVacio -stroke yellow -draw 'line 286,11 286,341' $LogVacio	# Despues del minuto 540
+magick $LogVacio -stroke yellow -draw 'line 316,11 316,341' $LogVacio	# Despues del minuto 600
+magick $LogVacio -stroke yellow -draw 'line 346,11 346,341' $LogVacio	# Despues del minuto 660
+magick $LogVacio -stroke yellow -draw 'line 376,11 376,341' $LogVacio	# Despues del minuto 720
+magick $LogVacio -stroke yellow -draw 'line 406,11 406,341' $LogVacio	# Despues del minuto 780
+magick $LogVacio -stroke yellow -draw 'line 436,11 436,341' $LogVacio	# Despues del minuto 840
+magick $LogVacio -stroke yellow -draw 'line 466,11 466,341' $LogVacio	# Despues del minuto 900
+magick $LogVacio -stroke yellow -draw 'line 496,11 496,341' $LogVacio	# Despues del minuto 960
+magick $LogVacio -stroke yellow -draw 'line 526,11 526,341' $LogVacio	# Despues del minuto 1020
+magick $LogVacio -stroke yellow -draw 'line 556,11 556,341' $LogVacio	# Despues del minuto 1080
+magick $LogVacio -stroke yellow -draw 'line 586,11 586,341' $LogVacio	# Despues del minuto 1140
+magick $LogVacio -stroke yellow -draw 'line 616,11 616,341' $LogVacio	# Despues del minuto 1200
+magick $LogVacio -stroke yellow -draw 'line 646,11 646,341' $LogVacio	# Despues del minuto 1260
+magick $LogVacio -stroke yellow -draw 'line 676,11 676,341' $LogVacio	# Despues del minuto 1320
+magick $LogVacio -stroke yellow -draw 'line 706,11 706,341' $LogVacio	# Despues del minuto 1380
+magick $LogVacio -stroke yellow -draw 'line 736,11 736,341' $LogVacio	# Despues del minuto 1440
+
